df62532dbf8be8c5344716b8e30f0e6df864668d
091CC <130974652+091cc@users.noreply.github.com>
2026-06-04 18:50:55 +0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,14 +1,11 @@
 @echo off
 title Keylogger Dependencies Installer
 
-echo [1/3] Installing required Python packages...
+echo [1/2] Installing required Python packages...
 py -m pip install pynput pywin32
 
-echo [2/3] Running pywin32 post-install configuration...
+echo [2/2] Running pywin32 post-install configuration...
 py -m pywin32_postinstall -install
-
-echo [3/3]Installing required Python packages...
-py -m pip install pygetwindow
 
 echo.
 echo ==========================================
